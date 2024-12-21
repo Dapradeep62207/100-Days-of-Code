5e3586beca6aab685563763ac7fa8e462f43a086
Pradeep Singh <149491734+Dapradeep62207@users.noreply.github.com>
2024-12-21 18:37:25 +0530

    Add files via upload

---
+++ Day_3 Logical Operators.py
@@ -0,0 +1,20 @@
+# A (and) B both (have to be true)
+
+# True and True -True
+# True and False - False
+# a = 12
+# a > 15
+# False
+
+# C (or) D False
+a = 12
+a > 10 or a < 10
+#True
+
+# True or True - True
+# True or False - True
+# False or True - True
+# False or False - False
+
+# Not operator is used to reverse the condion
+# not a < 0 True
